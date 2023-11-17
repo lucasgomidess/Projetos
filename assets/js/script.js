9317043c87385caae95083cde288a1768c3df9e5
Your Name <you@example.com>
2023-11-17 02:55:35 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const horas = document.getElementById("horas");
+const minutos = document.getElementById("minutos");
+const segundos = document.getElementById("segundos");
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    hr = hr < 10 ? "0" + hr : hr;
+    min = min < 10 ? "0" + min : min;
+    s = s < 10 ? "0" + s : s;
+
+ 
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
+    
+})
